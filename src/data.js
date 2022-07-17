a13a5eb422826b7734a973d6ec592e6193ffb4a6
Mujakkir85 <mujakkirlucse@gmail.com>
2022-07-17 23:15:39 +0600

    navbar and slider added

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://img.freepik.com/free-vector/people-carrying-shopping-bags-collectio_52683-19473.jpg?t=st=1658067250~exp=1658067850~hmac=3e1b84b0570d1ca77377a2bbd1d5911377e02dbcfa7dead5ec85b1029f88dc52&w=996",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://img.freepik.com/free-vector/people-carrying-shopping-bags-collectio_52683-19473.jpg?t=st=1658067250~exp=1658067850~hmac=3e1b84b0570d1ca77377a2bbd1d5911377e02dbcfa7dead5ec85b1029f88dc52&w=996",
+    title: "AUTUMN COLLECTION",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://img.freepik.com/free-vector/people-carrying-shopping-bags-collectio_52683-19473.jpg?t=st=1658067250~exp=1658067850~hmac=3e1b84b0570d1ca77377a2bbd1d5911377e02dbcfa7dead5ec85b1029f88dc52&w=996",
+    title: "LOUNGEWEAR LOVE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4",
+  },
+];
